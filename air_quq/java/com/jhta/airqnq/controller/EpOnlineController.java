b157292a58d43d96cd2891f10cd432ab1daa7e8c
testid <jwjs3219@gmail.com>
2020-07-16 09:32:14 +0900

    0716 fixing complete

--- air_quq/java/com/jhta/airqnq/controller/EpOnlineController.java
+++ air_quq/java/com/jhta/airqnq/controller/EpOnlineController.java
@@ -3,11 +3,9 @@ package com.jhta.airqnq.controller;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.List;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -15,7 +13,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-
 import com.jhta.airqnq.service.EpOnlineService;
 import com.jhta.airqnq.vo.ChatLogVo;
 
@@ -61,7 +58,7 @@ public class EpOnlineController {
 		int n=0;
 		try {
 			n=service.findRoom(hash);
-		}catch(NullPointerException ne){}
+		}catch(NullPointerException ne) {}
 		
 		if(n!=0) {//방 있음 ,내역 불러와야함
 			List<ChatLogVo> chat=service.chatlog(n);
